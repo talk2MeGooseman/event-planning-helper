4078f1b7d105cfd06a5147d9ebcb69702a82a3fa
Erik Guzman <erik.guzman@guzman.codes>
2021-07-29 23:55:01 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import {
+  add,
+  formatDistanceStrict,
+  lightFormat,
+  parseISO,
+  sub
+} from "date-fns";
+import { pipe } from "ramda";
+import { useCallback, useState } from "react";
+import ICalendarLink from "react-icalendar-link";
+import "./styles.css";
+
+const milestones = [
+  { duration: { months: 6 }, unit: "month" },
+  { duration: { months: 3 }, unit: "month" },
+  { duration: { days: 100 }, unit: "day" },
+  { duration: { days: 60 }, unit: "day" },
+  { duration: { days: 45 }, unit: "day" },
+  { duration: { days: 14 }, unit: "day" }
+];
+const postMilestones = [
+  { duration: { days: 14 }, unit: "day" },
+  { duration: { months: 2 }, unit: "month" },
+  { duration: { years: 1 }, unit: "year" }
+];
+
+const createEvent = ({ name, eventDate, milestoneDate, unit, isPost }) => {
+  const timeDistance = formatDistanceStrict(eventDate, milestoneDate, unit);
+
+  return {
+    title: `${name} ${timeDistance} ${isPost ? "post" : ""}`,
+    description: "",
+    startTime: milestoneDate,
+    endTime: milestoneDate,
+    location: ""
+  };
+};
+
+const Event = ({ name, eventDate, milestoneDate, unit, isPost }) => {
+  return (
+    <>
+      <h2>
+        {name} {formatDistanceStrict(eventDate, milestoneDate, { unit })}{" "}
+        {isPost && "post"} {lightFormat(milestoneDate, "MM-dd-yyyy")}
+      </h2>
+      <ICalendarLink
+        event={createEvent({
+          name,
+          eventDate: eventDate,
+          milestoneDate: milestoneDate,
+          unit,
+          isPost
+        })}
+      >
+        Add to Calendar
+      </ICalendarLink>
+    </>
+  );
+};
+
+export default function App() {
+  const [date, setDate] = useState("");
+  const [name, setName] = useState("");
+
+  const onDayChange = useCallback(
+    (event) => {
+      pipe(parseISO, setDate)(event.target.value);
+    },
+    [setDate]
+  );
+
+  const onNameChange = useCallback(
+    (event) => {
+      setName(event.target.value);
+    },
+    [setName]
+  );
+
+  return (
+    <div className="App">
+      <h1>Hey Sexy</h1>
+      <h2>Select a date to get your milestones</h2>
+      <label>
+        Client Name:
+        <input type="text" onChange={onNameChange} />
+      </label>
+      <label>
+        Event Date:
+        <input type="date" onChange={onDayChange} />
+      </label>
+      {date &&
+        milestones.map(({ duration, unit }) => (
+          <Event
+            id={name}
+            name={name}
+            eventDate={date}
+            milestoneDate={sub(date, duration)}
+            unit={unit}
+          />
+        ))}
+      {date &&
+        postMilestones.map(({ duration, unit }) => (
+          <Event
+            id={name}
+            name={name}
+            eventDate={date}
+            milestoneDate={add(date, duration)}
+            unit={unit}
+            isPost
+          />
+        ))}
+    </div>
+  );
+}
